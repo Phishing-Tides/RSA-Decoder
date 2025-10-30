12cd708ea76da7652204d098f8e63ebc009635fa
Phishing Tides <143620957+Phishing-Tides@users.noreply.github.com>
2025-10-30 17:43:53 +0000

    started working on rsa decoder

---
+++ decoder.py
@@ -0,0 +1,12 @@
+import rsa
+import math
+
+def rsa_decrypt():
+    print("Hello")
+
+def find_primes(p, q, n):
+    #for i in range(Add part where i++)
+    while n % i == 0
+
+n = 1079
+p, q = find_primes(p, q, n)
